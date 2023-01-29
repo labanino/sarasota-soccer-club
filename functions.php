79d02441ae1d8330d78141be7b353835cf842fc7
Alex Labanino <ghostrider6610@gmail.com>
2023-01-29 11:35:14 -0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+
+function boilerplate_load_assets()
+{
+    wp_enqueue_script('ourmainjs', get_theme_file_uri('/build/app.js'), array('wp-element'), '1.0', true);
+    wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/style.css'));
+}
+
+add_action('wp_enqueue_scripts', 'boilerplate_load_assets');
+
+function boilerplate_add_support()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'boilerplate_add_support');
+
+// register the nav
+function register_my_menu() {
+        register_nav_menus( array(
+            'main-menu' => __( 'Main Menu', 'text_domain' ),
+            'footer'  => __( 'Footer ', 'text_domain' ),
+        )
+    );
+}
+add_action( 'init', 'register_my_menu' );
+
+// let's add "*active*" as a class to the li
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+function special_nav_class($classes, $item){
+    if( in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
+
+// let's add our custom class to the actual link tag
+function atg_menu_classes($classes, $item, $args) {
+    if($args->theme_location == 'main-menu') {
+        $classes[] = '';
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'atg_menu_classes', 1, 3);
+
+function add_menuclass($ulclass) {
+    return preg_replace('/<a /', '<a class="menuButton"', $ulclass);
+}
+add_filter('wp_nav_menu','add_menuclass');
---
+++ homepage.php
@@ -0,0 +1,84 @@
+<?php
+/* Template Name: Homepage */
+get_header(); ?>
+
+    <!-- HOURS -->
+    <section class="hours">
+        <div class="grid grid-cols-12">
+            <span class="2md:col-span-4 sm:col-span-6 xs:col-span-full border-b-1 border-b border-middleGray flex flex-col sm:items-end xs:items-center justify-center md:py-12 md:pr-10 md:pl-2 sm:pr-10 xs:px-2 xs:py-12 sm:text-right xs:text-center">
+                <h2 class="font-extrabold xl:text-3xl xs:text-[1.2rem] uppercase leading-h2 mb-3 text-darkRed">Monday night clinics<br>& saturday games</h2>
+                <p class="xl:text-xl xs:text-sm font-extralight uppercase text-darkRed">Mon 5:30 - 7:00 pm | Sat 9:00 - 11:00 am</p>
+            </span>
+            <div class="2md:col-span-3 sm:col-span-6 bg-center bg-cover -mt-10 h-55 sm:visible xs:invisible bg-[url('/wp-content/themes/sarasota-soccer-club/images/stadium-sits.jpg')]"></div>
+            <div class="md:col-span-5 bg-center bg-cover -mt-10 h-55 2md:visible xs:invisible bg-[url('/wp-content/themes/sarasota-soccer-club/images/soccer-ball.jpg')]"></div>
+        </div>
+    </section>
+    <!-- end HOURS -->
+
+    <!-- ABOUT -->
+    <section class="about text-darkRed leading-6 md:flex">
+        <div class=" md:flex md:w-1/2">
+            <img class="w-auto md:object-right md:h-auto sm:h-64 xs:h-44 xs:w-full object-cover" src="/wp-content/themes/sarasota-soccer-club/images/about.jpg" alt="" />
+        </div>
+        <div class="md:w-1/2 2md:pr-32 md:flex md:flex-col md:justify-center xs:w-full xs:px-6 xs:py-12">
+            <h2 class="font-extrabold xl:text-3xl xs:text-[1.2rem] uppercase leading-h2 mb-3">Soccer to the masses</h2>
+            <p class="font-extralight leading-lead md:text-2base xs:text-base">The <strong>Sarasota City Soccer Club</strong> was founded in 2017 as a 501(c)(3) nonprofit corporation, focused on opening up opportunities for soccer lovers of all backgrounds. Kids will be taught the values of <strong>teamwork</strong>, <strong>respect</strong>, potential, and a <strong>healthy life</strong> model while cultivating an appreciation for sports in a fun and <strong>safe environment</strong>. <a href="#"><img class="inline-flex items-baseline" src="/wp-content/themes/sarasota-soccer-club/images/red-arrow.svg" alt="" title="Safe environment"></a></p>
+        </div>
+    </section>
+    <!-- end ABOUT -->
+
+    <!-- SCHOLARSHIPS -->
+    <section class="scholarship text-white leading-6 relative">
+        <div class="absolute inset-0 bg-lightPurple bg-opacity-95"></div>
+        <div class="flex bg-[url('/wp-content/themes/sarasota-soccer-club/images/scholarship-bg.jpg')] bg-no-repeat bg-cover bg-center xs:px-6 xs:py-12 xs:w-full">
+            <div class="2md:w-1/2 z-10 2md:ml-36 flex flex-col justify-center">
+                <h2 class="font-extrabold xl:text-3xl xs:text-[1.2rem] uppercase leading-h2 mb-3">Scholarship</h2>
+                <span class="font-extralight leading-lead md:text-2base xs:text-base">
+                    <p class="mb-3">In order to maximize the positive effect of our initiatives, we award scholarships to those who cannot otherwise afford the registration fee.</p>
+                    <p>If you would like to apply for a scholarship, be sure to fill out the <strong>scholarship form</strong> completely by sending us the all-important details of the application. <a href="#"><img class="inline-flex items-baseline" src="/wp-content/themes/sarasota-soccer-club/images/white-arrow.svg" alt="" title="Safe environment"></a></p>
+                </span>
+            </div>
+            <div class="flex 2md:w-1/2 z-10">
+                <img class="-mt-48 2md:block xs:hidden" src="/wp-content/themes/sarasota-soccer-club/images/young-player.png" alt="" />
+            </div>
+        </div>
+    </section>
+    <!-- end SCHOLARSHIPS -->
+
+    <!-- SPONSORS -->
+    <section class="sponsors container mx-auto">
+        <div class="flex justify-center items-center 2md:space-x-10 xs:space-x-5 py-7">
+            <span class="uppercase text-darkBlue font-poppins font-extrabold md:text-[1.50rem] xs:text-base">
+                <a href="#">Be a sponsor today <img class="inline-flex md:hidden xs:items-baseline" src="/wp-content/themes/sarasota-soccer-club/images/dark-blue-arrow.svg" alt="" title="Safe environment"></a>
+            </span>
+            <span class="md:block xs:hidden"><a href="#"><img src="/wp-content/themes/sarasota-soccer-club/images/AO-Impact-Logo.png" alt="AO Impact" title="AO Impact"></a></span>
+            <span class="md:block xs:hidden"><a href="#"><img src="/wp-content/themes/sarasota-soccer-club/images/shamrock.png" alt="The Shamrock" title="The Shamrock"></a></span>
+            <span class="md:block xs:hidden"><a href="#"><img src="/wp-content/themes/sarasota-soccer-club/images/digital-marketing-srq.png" alt="Digital Marketing SRQ" title="Digital Marketing SRQ"></a></span>
+        </div>
+    </section>
+    <!-- end SPONSORS -->
+
+    <!-- PICTURES BLOCK -->
+    <section class="soccer-pictures grid grid-cols-3">
+        <div class="h-[32rem] bg-center bg-cover bg-[url('/wp-content/themes/sarasota-soccer-club/images/soccer1.jpg')]"></div>
+        <div class="h-[32rem] bg-center bg-cover bg-[url('/wp-content/themes/sarasota-soccer-club/images/soccer2.jpg')]"></div>
+        <div class="h-[32rem] bg-center bg-cover bg-[url('/wp-content/themes/sarasota-soccer-club/images/soccer3.jpg')]"></div>
+    </section>
+    <!-- end PICTURES BLOCK -->
+
+    <!-- VOLUNTEER -->
+    <section class="about text-darkRed leading-6">
+        <div class="md:flex bg-[url('/wp-content/themes/sarasota-soccer-club/images/volunteer-bg.png')] bg-no-repeat bg-cover bg-center">
+            <div class=" md:flex md:w-2/5 md:p-8 xs:p-5">
+                <img class="mx-auto" src="/wp-content/themes/sarasota-soccer-club/images/coach.png" alt="" />
+            </div>
+            <div class="md:w-3/5 2md:pr-32 md:flex xs:flex-col md:justify-center xs:px-6 xs:pt-4 xs:pb-12">
+                <h2 class="font-extrabold xl:text-3xl xs:text-[1.2rem] uppercase leading-h2 mb-3">Volunteer</h2>
+                <p class="font-extralight leading-lead md:text-2base xs:text-base mb-3"><strong>Sarasota City Soccer Club</strong> offers players between the ages of 5 and 12 the chance to try out our Recreational and Competitive teams.</p>
+                <p class="font-extralight leading-lead md:text-2base xs:text-base">We focus on offering a <strong>fun</strong> and <strong>safe learning environment</strong> for all players, their parents, and coaches, so that everyone understands the rules of the game. Contact us to <strong>volunteer</strong>. <a href="#"><img class="inline-flex items-baseline" src="/wp-content/themes/sarasota-soccer-club/images/red-arrow.svg" alt="" title="Volunteer"></a></p>
+            </div>
+        </div>
+    </section>
+    <!-- end VOLUNTEER -->
+
+<?php get_footer();
